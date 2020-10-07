d3579240a151bd216a9223856fd123c73b59e654
Brice Bezin <bezinbrice@gmail.com>
2020-10-07 10:17:33 +0200

    Final project
    React app

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import ReactAux from '../../../hoc/ReactAux/ReactAux';
+
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close]
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
+    return (
+        <ReactAux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav onClick={props.closed}>
+                    <NavigationItems isAuthenticated={props.isAuth}/>
+                </nav>
+            </div>
+        </ReactAux>
+    );
+};
+
+export default sideDrawer;
